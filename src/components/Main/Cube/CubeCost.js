140704ca45ead020b7097c271f4e763bccc26bef
jungpyolee <80612689+jungpyolee@users.noreply.github.com>
2021-06-09 21:54:27 +0900

    큐브개수, 금액css 수정

--- src/components/Main/Cube/CubeCost.js
+++ src/components/Main/Cube/CubeCost.js
@@ -1,11 +1,25 @@
 import React from "react";
-
+import "../../../stylesheet/CubeCost.css";
+import { Col, Row } from "antd";
 function CubeCost(props) {
   return (
-    <div>
-      총{props.count}개
-      <br />
-      {props.price}원
+    <div className="cubeCost_body">
+      <div className="cubeCost">
+        <Row gutter={[16, 16]}>
+          <Col lg={12} xs={24}>
+            사용 큐브
+          </Col>
+          <Col lg={12} xs={24}>
+            총&nbsp;{props.count}&nbsp;개
+          </Col>
+          <Col lg={12} xs={24}>
+            사용 금액
+          </Col>
+          <Col lg={12} xs={24}>
+            {props.price.toLocaleString()}원
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
